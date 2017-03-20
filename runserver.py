18c54971b6ff8f1b735c665aebc5ce8fea95ea7f
stamigos <oldtigersvoice@gmail.com>
2017-03-20 21:10:51 +0200

    switched port to 8000

--- runserver.py
+++ runserver.py
@@ -1,5 +1,5 @@
 from radar import app, socketio
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=8008, debug=True)
+    socketio.run(app, host='0.0.0.0', port=8000, debug=True)
     # app.run(host='0.0.0.0', debug=True, port=8008, threaded=True)
